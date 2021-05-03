1312a64586c86204a7fc4080b7e38ee5d70d1917
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-05-03 10:48:37 +0700

    init

---
+++ MobileApp2/MobileApp2/MobileApp2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      //  private void Button_Clicked(object sender, EventArgs e)
+      //  {
+
+      //  }
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
